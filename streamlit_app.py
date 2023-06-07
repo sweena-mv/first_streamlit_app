26b7da19cee2a34321935e5aab1a2eca5007b424
sweena-mv <86863301+sweena-mv@users.noreply.github.com>
2023-06-07 13:08:07 +0930

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
-streamlit.title("My Healthy dinner")
-streamlit.header("Beakfast menu")
-streamlit.text("Omega 3 & Blueberry Oatmeal")
-streamlit.text("Kale, Spinach & Rocket Smoothie")
-streamlit.text("Hard-Boiled Free-Range Egg")
+streamlit.title('My Healthy dinner')
+streamlit.header('Beakfast menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text'Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
